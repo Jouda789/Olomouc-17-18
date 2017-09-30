b48763554fb90f2472459d8770d666bae72fd1d4
Jouda789 <j606940@gmail.com>
2017-09-30 21:59:25 +0200

    Jenom kosmeticke upravy

--- Papr1-DU1/cv2.rkt
+++ Papr1-DU1/cv2.rkt
@@ -2,16 +2,7 @@
 (define y 3)
 (define z 2)
 
-(if (= y 4)
-    (* y z)
-    (if (= y 2)
-        (* x z)
-        (if (= z 4)
-            (+ x y z)
-            (* x z))))
-
 (cond ((= y 4) (* y z))
       ((= y 2) (* x y))
-      ((= z 4) (+ z x y ))
+      ((= z 4) (+ x y z))
       (else (* z x)))
-           

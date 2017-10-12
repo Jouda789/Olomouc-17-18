e18655ca9dfdd3179dd449985842f8d5c04cde64
Judita Kindlová <j606940@gmail.com>
2017-10-12 12:27:35 +0200

    Napište program, který načte ze vstupu dvě čísla a vypíše jejich součet, rozdíl a součin.

--- ZP1 - cviceni ze stranek/lekce 2/8.c
+++ ZP1 - cviceni ze stranek/lekce 2/8.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* 8. Napište program, kterı naète ze vstupu dvì èísla a vypíše jejich souèet, rozdíl a souèin. */
+/* 8. NapiÅ¡te program, kterÃ½ naÄte ze vstupu dvÄ› ÄÃ­sla a vypÃ­Å¡e jejich souÄet, rozdÃ­l a souÄin. */
 
 int main(){
-    int a,b;
+    int a, b;
     scanf("%d %d", &a, &b);
     printf("soucet je %d, rozdil je %d, soucin je %d", a+b,a-b,a * b);
 }

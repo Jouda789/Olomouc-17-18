e651070bea416061fac49bbf10005710d470ba6c
Judita Kindlová <j606940@gmail.com>
2017-11-16 10:52:34 +0100

    novy adresar

--- ZP1/lekce 5 (funkce)/4 - Napište program, který pro dané přirozené číslo n vypíše všechna prvočísla menší než n, jejichž součet číslic je také prvočíslem..c
+++ ZP1/lekce 5 (funkce)/4 - Napište program, který pro dané přirozené číslo n vypíše všechna prvočísla menší než n, jejichž součet číslic je také prvočíslem..c
@@ -1,49 +1,35 @@
 #include <stdio.h>
 #include <math.h>
 
-/* 4. Napište program, kterı pro dané pøirozené èíslo n vypíše všechna prvoèísla menší ne n, jejich souèet èíslic je také prvoèíslem. */
-/* nekde je chyba */
+/* 4. NapiÅ¡te program, kterÃ½ pro danÃ© pÅ™irozenÃ© ÄÃ­slo n vypÃ­Å¡e vÅ¡echna prvoÄÃ­sla menÅ¡Ã­ neÅ¾ n, jejichÅ¾ souÄet ÄÃ­slic je takÃ© prvoÄÃ­slem. */
 
-int JePrvocislo(int i){
-    
+int prime(int i)
+{
     int a = sqrt(i);
     int j;
     if (i == 1) return 0;
     if (i == 2) return 1;
     if ((i % 2) == 0) return 0;
     for (j=3;j<=a;j++){
-        if ((i % j) == 0)  return 0; 
-        else return 1; 
-    }    
+        if ((i % j) == 0)  return 0;  
+    }      
+    return 1;
 }
 
-int SoucetJePrvocislo(int cislo){
-    
-    int pom = cislo, pocetCifer = 0;
-    int i, soucet = 0;
-    while (pom > 0) {
-        pocetCifer++;
-        pom /= 10;
+int soucet_cislic(int n)
+{
+    int soucet = 0;
+    while(n)
+    {
+        soucet += n%10;
+        n /= 10;            
     }
-    
-    for(i=0;i<pocetCifer;i++){
-        pom = cislo % 10;
-        soucet += pom;
-        cislo /= 10;       
-    }
-    
-    if (JePrvocislo(soucet)) return 1;
-    else return 0; 
+   return soucet;        
 }
 
-int main() {
-	
- int n;
- scanf("%d", &n);
- int i;
- for (i=2;i<n;i++){
-     
-     if (JePrvocislo(n) && SoucetJePrvocislo(n)) printf("%d ", n);    
- }         
+int main(){
+    int n, i;
+    scanf("%d", &n);
+    for (i=2;i<=n;i++)
+    if (prime(i) && (prime(soucet_cislic(i)))) printf("%d ", i);
 }
-

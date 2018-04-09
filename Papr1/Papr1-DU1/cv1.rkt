a1b532404e1bc10ee95a520bd69016612ae6c84b
Jouda789 <j606940@gmail.com>
2018-04-09 19:54:28 +0200

    novy adresar

---
+++ Papr1/Papr1-DU1/cv1.rkt
@@ -0,0 +1,9 @@
+(define y 2)
+(define x 3)
+
+(define a (+ 1 (/ 1 y) (/ 1 (* y y))(/ 1 (* y y y))))
+(define b (/ ( * (expt 3 y) x ) (expt 2 x)))
+(define c (sqrt (+ b (* x a) )))
+(define d (sqrt (+ (* 2 y) (* y y) (* y y y))))
+
+(sqrt (/ (* c x) d))
